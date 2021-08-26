76b45cd593c54d7c72830fda8902075cee919a4c
Luis Claudio Soto Soto Ayala <claudio.soto.ayala@gmail.com>
2021-08-26 16:06:57 -0500

    Full Refactoring chapter done

--- CleanCode/CleanCode/FullRefactoring/Example1.cs
+++ CleanCode/CleanCode/FullRefactoring/Example1.cs
@@ -5,70 +5,88 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web.UI.WebControls;
 
-namespace Project.UserControls
+namespace CleanCode.FullRefactoring
 {
     public class PostControl : System.Web.UI.UserControl
     {
-        private PostDbContext DBContext;
+        private readonly Project.UserControls.PostRepository _postRepository;
+        private readonly PostValidator _validator;
+        private Label PostBody { get; set; }
+        private Label PostTitle { get; set; }
+        private int? PostId { get; set; }
 
+        public PostControl()
+        {
+            _postRepository = new Project.UserControls.PostRepository();
+            _validator = new PostValidator();
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            DBContext = new PostDbContext();
 
             if (Page.IsPostBack)
+            
+                TrySavePost();
+            else
+                DisplayPost();
+        }
+
+        private void TrySavePost()
+        {
+            var post = CreateNewPost();
+            var results = _validator.Validate(post);
+
+            if (results.IsValid)
             {
-                PostValidator validator = new PostValidator();
-                Post entity = new Post()
-                {
-                    // Map form fields to entity properties
-                    Id = Convert.ToInt32(PostId.Value),
-                    Title = PostTitle.Text.Trim(),
-                    Body = PostBody.Text.Trim()
-                };
-                ValidationResult results = validator.Validate(entity);
-
-                if (results.IsValid)
-                {
-                    // Save to the database and continue to the next page
-                    DBContext.Posts.Add(entity);
-                    DBContext.SaveChanges();
-                }
-                else
-                {
-                    BulletedList summary = (BulletedList)FindControl("ErrorSummary");
-
-                    // Display errors to the user
-                    foreach (var failure in results.Errors)
-                    {
-                        Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
-
-                        if (errorMessage == null)
-                        {
-                            summary.Items.Add(new ListItem(failure.ErrorMessage));
-                        }
-                        else
-                        {
-                            errorMessage.Text = failure.ErrorMessage;
-                        }
-                    }
-                }
+                _postRepository.SavePost(post);
             }
             else
             {
-                // Display form
-                Post entity = DBContext.Posts.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
-                PostBody.Text = entity.Body;
-                PostTitle.Text = entity.Title;
-
+                DisplayErrors(results);
             }
         }
 
-        public Label PostBody { get; set; }
+        private void DisplayPost()
+        {
+            int postId = Convert.ToInt32(Request.QueryString["id"]);
+            var post = _postRepository.GetPost(postId);
+            PostBody.Text = post.Body;
+            PostTitle.Text = post.Title;
+        }
 
-        public Label PostTitle { get; set; }
+        private Post CreateNewPost()
+        {
+            return new Post()
+            {
+                Id = Convert.ToInt32(PostId.Value),
+                Title = PostTitle.Text.Trim(),
+                Body = PostBody.Text.Trim()
+            };
+        }
 
-        public int? PostId { get; set; }
+        private void DisplayErrors(ValidationResult results)
+        {
+            var errorLog = GetSummaryErrorControl();
+
+            foreach (var error in results.Errors)
+            {
+                if (!GetErrorLabel(error, out var errorMessage)) 
+                    errorLog.Items.Add(new ListItem(error.ErrorMessage));
+                else
+                    errorMessage.Text = error.ErrorMessage;
+            }
+        }
+        private BulletedList GetSummaryErrorControl()
+        {
+            return (BulletedList)FindControl("ErrorSummary");
+        }
+
+        private bool GetErrorLabel(ValidationError error, out Label label)
+        {
+            label = null;
+            return FindControl(error.PropertyName + "Error") is Label;
+        }
+
     }
 
     #region helpers
